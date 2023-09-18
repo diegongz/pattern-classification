2bc529050d70fea0f6196b956a2428460f4a03ea
Diego Noguez <diego_nogez@hotmail.com>
2023-09-18 10:38:55 -0600

    Last commit
    In this commit I upload all the python work i've done in pattern classification subjet.

---
+++ data_sets/clean.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+
+df=data.frame(id=c(11,11,33,44,44),
+              pages=c(32,32,33,22,22),
+              name=c("spark","spark","R","java","jsp"),
+              chapters=c(76,76,11,15,15),
+              price=c(144,144,321,567,567))
+
